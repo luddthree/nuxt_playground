ce087bd028837371c71d03bc91b4c9d97897f35a
luddthree <luddetv@gmail.com>
2023-09-18 14:24:47 +0200

    ye

---
+++ storefront/pages/products/[id].vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <p>product details for {{ id }}</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque repudiandae aliquam culpa. Reprehenderit eveniet obcaecati incidunt velit delectus accusantium et iste, soluta, laudantium voluptas, consectetur modi repellendus deserunt at vero.
+        </p>
+    </div>
+</template>
+
+<script setup>
+    const { id } = useRoute().params
+</script>
