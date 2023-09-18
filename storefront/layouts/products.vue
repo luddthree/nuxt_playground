48083f53bb4ff0d754be4be52af32e8331ad8e83
luddthree <luddetv@gmail.com>
2023-09-18 14:48:46 +0200

    id

---
+++ storefront/layouts/products.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <header>
+            <nav>
+                <NuxtLink to="/products">Nuxt dojo merch</NuxtLink>
+
+            </nav>
+        </header>
+
+        <div>
+            <slot />
+        </div>
+
+        <footer>
+            <ul>
+                    <li><NuxtLink to="/">Home</NuxtLink></li>
+                    <li><NuxtLink to="/about">About</NuxtLink></li>
+                    <li><NuxtLink to="/products">Products</NuxtLink></li>
+                </ul>
+        </footer>
+    </div>
+</template>
+
+<style scoped>
+    .router-link-exact-active {
+        color: #12b488;
+    }
+</style>
