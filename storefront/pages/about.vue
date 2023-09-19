62a8f52ecc8f71a471a17cd50d7feba875a123ee
luddthree <luddetv@gmail.com>
2023-09-19 13:52:08 +0200

    hello mario you are 30 years old server exsample

--- storefront/pages/about.vue
+++ storefront/pages/about.vue
@@ -1,11 +1,17 @@
 <template>
     <div>
       <h1>About</h1>
+
+
+      <div> {{ data }}</div>
     </div>
 </template>
 
 <script setup>
-
+  const { data } = await useFetch('/api/storefront?name=mario', {
+    method: 'POST',
+    body: {age: 30}
+  })
 </script>
 
 <style scoped>
---
+++ storefront/server/api/storefront.js
@@ -0,0 +1,12 @@
+export default defineEventHandler(async(event) => {
+
+
+
+    const { name } = getQuery(event)
+
+    const { age } = await readBody(event)
+
+    return {
+        message: `hello, ${name}! you are ${age} years old`
+    }
+})
